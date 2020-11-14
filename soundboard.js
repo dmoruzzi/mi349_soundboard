aa7fb9d52fb9906c625333f6afa1733f5f9a267b
David Moruzzi <dmoruzzi@users.noreply.github.com>
2020-11-14 04:37:24 -0500

    Init

---
+++ soundboard.js
@@ -0,0 +1,28 @@
+
+document.getElementById("vending-coin").onclick = function () {
+    document.getElementById("vending-coin-audio").play();
+};
+
+document.getElementById("explosion").onmouseenter = function () {
+    document.getElementById("explosion-audio").play();
+};
+
+document.getElementById("explosion").onclick = function () {
+    document.getElementById("explosion-audio").play();
+};
+
+document.getElementById("honk").onmouseenter = function () {
+    document.getElementById("honk-audio").play();
+};
+
+document.getElementById("honk").onclick = function () {
+    document.getElementById("honk-audio").play();
+};
+
+document.getElementById("tuner").onmouseenter = function () {
+    document.getElementById("tuner-audio").play();
+};
+
+document.getElementById("tuner").onclick = function () {
+    document.getElementById("tuner-audio").play();
+};
